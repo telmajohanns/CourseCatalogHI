ce8ab3793cdc6b11112a7115d81683089f88dd84
telmajohanns <tej2@hi.is>
2023-03-12 20:37:59 +0000

    First commit

---
+++ app/src/main/java/com/example/coursecataloghi/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.coursecataloghi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button loginbutton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        loginbutton = (Button) findViewById(R.id.btnlogin);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Græja hvað gerist þegar ýtt er á login
+            }
+        });
+
+
+    }
+}
