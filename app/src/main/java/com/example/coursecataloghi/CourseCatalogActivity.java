56391a77814fcfabee3fc8336e2d8ca0ebdabf9d
steinunnmaria <steinunnmaria96@gmail.com>
2023-03-13 00:14:29 +0000

    -

--- app/src/main/java/com/example/coursecataloghi/CourseCatalogActivity.java
+++ app/src/main/java/com/example/coursecataloghi/CourseCatalogActivity.java
@@ -1,17 +1,36 @@
 package com.example.coursecataloghi;
 
 import android.os.Bundle;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CourseCatalogActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import Entities.Course;
+
+public class CourseCatalogActivity extends AppCompatActivity {
+    ExpandableListView courseCatalogList;
+    private ExpandableListAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_catalog);
+        courseCatalogList = (ExpandableListView) findViewById(R.id.courseCatalogList);
+        //Þurfum að gera adapter og eitthvað allskonar til að geta sett hluti í listviewið
 
+    }
 
+    private ArrayList<Course> createCourseCatalog() {
+        ArrayList<Course> courses = new ArrayList<>();
+        ArrayList<String> titles = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            titles.add("TÖL" + String.valueOf(i));
+            Course course = new Course("TÖL" + String.valueOf(i), "Tölvunarfræði " + String.valueOf(i), 6.0, "Vor", "Grunnnám", "VON", "IVT", "IS", "Ebba Þóra", "Steinunn María", "2022-2023", true, "1234", "linkur");
+            courses.add(course);
+        }
+        return courses;
     }
 }
